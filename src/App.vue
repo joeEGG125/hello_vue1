6bb07f8b373e7d27e765137f3c875d78a1ce8702
joeEGG125 <joey314@gmail.com>
2024-02-26 15:27:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<componentA></componentA>
+</template>
+
+
+
+<script setup>
+import componentA from './components/componentA.vue';
+</script>
+
+
+
+<style>
+
+</style>
---
+++ src/components/componentA.vue
@@ -0,0 +1,23 @@
+<template>
+<p>Hello Vue</p>
+{{ data }}
+
+<p>--</p>
+<input type="text" v-model="text">
+<p>{{ text }}</p>
+</template>
+
+
+
+<script setup>
+import { ref } from 'vue';
+
+const data = "hello Vue 3";
+const text = ref("555");
+</script>
+
+
+
+<style>
+
+</style>
